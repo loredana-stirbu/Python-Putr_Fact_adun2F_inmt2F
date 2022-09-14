dbb0c8b872dc054b78c00eaf821e1d050668df2c
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2022-09-15 01:34:36 +0300

    Add files via upload

---
+++ lista_int.py
@@ -0,0 +1,29 @@
+def creare_lista_int():
+    n = int(input('Numarul de elemente: '))
+    lista_locala = []
+    for i in range(n):
+        element = input(f'Elementul {i+1} este: ')
+        try:
+            lista_locala.append(int(element))
+        except ValueError:
+            while not element.isnumeric():
+                element=input("Introdu un nr. int:")
+            lista_locala.append(int(element))
+    return lista_locala
+    
+print(creare_lista_int())
+
+def creare_lista_float():
+    n = int(input('Numarul de elemente: '))
+    lista_locala = []
+    for i in range(n):
+        element = input(f'Elementul {i+1} este: ')
+        try:
+            lista_locala.append(float(element))
+        except ValueError:
+            while not element.isnumeric():
+                element=input("Introdu un nr. int:")
+            lista_locala.append(float(element))
+    return lista_locala
+    
+print(creare_lista_float())
